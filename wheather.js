4204c1111d47d664067d05ecfb45aaee4f926740
sait <sait.bozteke63@gmail.com>
2022-03-15 04:13:37 +0100

    file added

--- wheather.js
+++ wheather.js
@@ -3,7 +3,7 @@ const input = document.querySelector("div.container input");
 const msg = document.querySelector("span.msg");
 const cityList = document.querySelector(".ajax-section .cities");
 
-localStorage.setItem("apiKey", EncryptStringAES("4d8fb5b93d4af21d66a2948710284366"));
+localStorage.setItem("apiKey", EncryptStringAES("c628c5d84e0cebf2ba773adddcb2ab3d"));
 
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
